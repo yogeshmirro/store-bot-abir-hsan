187e68e64af3fb3201f1810236c5b367437daefb
yogeshmirro <101546464+yogeshmirro@users.noreply.github.com>
2022-11-21 06:24:37 +0530

    Create deta.sh

---
+++ deta.sh
@@ -0,0 +1,14 @@
+{
+    "name": "pyrobot",
+    "description": "tesying", 
+    "runtime": "python3.9",
+    "env": [
+        {
+            "key": "PORT",
+            "description": "Human friendly description of the env var",
+            "value": "8080",
+            "required": false 
+        }
+    ],
+    "cron": "3"
+}
