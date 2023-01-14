3d8c382e09110be6848e44dec3ce9fc0ef73c6a0
yogeshmirro <101546464+yogeshmirro@users.noreply.github.com>
2023-01-14 22:42:59 +0530

    Update remove_word.py

--- handlers/remove_word.py
+++ handlers/remove_word.py
@@ -1,12 +1,15 @@
+import re
 from configs import Config
 async def rmw(cap):
-    for i in Config.REMOVE_WORD:
-        if i in cap:
-            try:
-                cap1 = cap.replace(f"{i}","")
-                cap = cap1
-            except:
-                pass
-        else:
-            continue
-    return cap
+    caps = re.sub(Config.REMOVE_WORD,"",cap)
+    return caps
+#     for i in Config.REMOVE_WORD:
+#         if i in cap:
+#             try:
+#                 cap1 = cap.replace(f"{i}","")
+#                 cap = cap1
+#             except:
+#                 pass
+#         else:
+#             continue
+#     return cap
