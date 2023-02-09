97d532fc98d1ff6b9e48585b94886166a3ffca23
yogeshmirro <101546464+yogeshmirro@users.noreply.github.com>
2023-02-09 23:06:29 +0530

    Update check_user_status.py

--- handlers/check_user_status.py
+++ handlers/check_user_status.py
@@ -12,7 +12,7 @@ async def handle_user_status(bot, cmd):
     if not await db.is_user_exist(chat_id):
         await db.add_user(chat_id)
         await bot.send_message(
-            Config.LOG_CHANNEL,
+            int(Config.LOG_CHANNEL),
             f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{Config.BOT_USERNAME} !!"
         )
 
