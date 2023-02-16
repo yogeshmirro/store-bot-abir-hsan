4fffbcba2e9ddac6674689c4ce62ac6b5058e3b2
yogeshmirro <101546464+yogeshmirro@users.noreply.github.com>
2023-02-16 20:01:28 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -18,25 +18,25 @@ class Config(object):
   DATABASE_URL = os.environ.get("DATABASE_URL","")
   VERIFY_KEY = os.environ.get("VERIFY_KEY", "").split()#set plz don't use special character like @#$%* etc us only letter number and = ,To use this EARNING var value must be true.multiple key separted by space,if u want to use pre shorted link then verify_key and verify_link must be at same index. example if 'unfhd" this key is related to https://www.shorted.link then and verify_key is at index 1 in string(separated by space) then verify_link must be at index 1 in verify_link string
   VERIFY_LINK = os.environ.get("VERIFY_LINK","").split()#To use this EARNING var value must be True. multiple verification link separted by space. these links can be shorted links which is all related to VERIFY_KEY . Which link you want to short ,must be in this formate -- https://t.me/YOUR BOT USERNAME without @?start=verifylink_any text which u want to add
-  EARNING = bool(os.environ.get("EARNING",False))#If this is True then ---- 1.SHORTNER_API_LINK and SHORTNER_API must be fill if VERIFY_KEY not filled, 2. if VERIFY_KEY is filled then SHORTNER_API & SHORTNER_API_LINK must not filled
+  EARNING = os.environ.get("EARNING","False")#If this is True then ---- 1.SHORTNER_API_LINK and SHORTNER_API must be fill if VERIFY_KEY not filled, 2. if VERIFY_KEY is filled then SHORTNER_API & SHORTNER_API_LINK must not filled
   VERIFY_DURATION = (os.environ.get("VERIFY_DURATION",""))
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
   LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "")
   BANNED_USERS = set(
     int(x) for x in os.environ.get("BANNED_USERS", "1236848594").split())
-  FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
-  BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
+  FORWARD_AS_COPY = os.environ.get("FORWARD_AS_COPY", "True")
+  BROADCAST_AS_COPY = os.environ.get("BROADCAST_AS_COPY", "True")
   BANNED_CHAT_IDS = list(
     set(
       int(x)
       for x in os.environ.get("BANNED_CHAT_IDS", "-1007947838838").split()))
-  OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
+  OTHER_USERS_CAN_SAVE_FILE = os.environ.get("OTHER_USERS_CAN_SAVE_FILE", "False")#value should be in True or False
   SHORTNER_API_LINK = os.environ.get("SHORTNER_API_LINK", None)
   SHORTNER_API = os.environ.get("SHORTNER_API", None)
   REMOVE_WORD = os.environ.get("REMOVE_WORD","")#multiple word must be separted by '|'
   SEND_PHOTO = os.environ.get("SEND_PHOTO", "")
   ADD_DETAIL = os.environ.get("ADD_DETAIL", "")
-  SHORT_SINGLE_LINK = bool(os.environ.get("SHORT_SINGLE_LINK",False))#if u want every msg link in shorted formate then set it True . If it is True then SHORTNER_API and SHORTNER_API_LINK must required.
+  SHORT_SINGLE_LINK = os.environ.get("SHORT_SINGLE_LINK","False")#value should be in True or False if u want every msg link in shorted formate then set it True . If it is True then SHORTNER_API and SHORTNER_API_LINK must required.
   HOW_TO_VERIFY_LINK = os.environ.get("HOW_TO_VERIFY_LINK","")
   ABOUT_BOT_TEXT = f"""
 This is Permanent Files and text Store Bot!
