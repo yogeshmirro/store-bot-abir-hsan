adcd337987aeb9f1cc2c70db62336455551bdfcb
yogeshmirro <101546464+yogeshmirro@users.noreply.github.com>
2023-02-16 07:52:15 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -6,8 +6,7 @@ from pyrogram import Client
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait
 from handlers.helpers import str_to_b64
-
-
+from handlers.MultiChannel import get_db_channel
 # async def reply_forward(message: Message, file_id: int):
 #     try:
 #         await message.reply_text(
@@ -20,20 +19,20 @@ from handlers.helpers import str_to_b64
 #         await reply_forward(message, file_id)
 
 
-async def media_forward(bot: Client, user_id: int, file_id: int):
+async def media_forward(bot: Client, user_id: int, file_id: int, chat_id: int):
     try:
         if Config.FORWARD_AS_COPY is True:
-            return await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
+            return await bot.copy_message(chat_id=user_id, from_chat_id=chat_id,
                                           message_id=file_id)
         elif Config.FORWARD_AS_COPY is False:
-            return await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
+            return await bot.forward_messages(chat_id=user_id, from_chat_id=chat_id,
                                               message_ids=file_id)
     except FloodWait as e:
         await asyncio.sleep(e.value)
         return media_forward(bot, user_id, file_id)
 
 
-async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
-    sent_message = await media_forward(bot, user_id, file_id)
+async def send_media_and_reply(bot: Client, user_id: int, file_id: int, chat_id: int):
+    sent_message = await media_forward(bot, user_id, file_id, chat_id)
     #await reply_forward(message=sent_message, file_id=file_id)
     await asyncio.sleep(2)
